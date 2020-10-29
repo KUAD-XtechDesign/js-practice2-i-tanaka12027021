065c576a45965541502464ab7c46f3ab224dc74b
i-tanaka12027021 <68629502+i-tanaka12027021@users.noreply.github.com>
2020-10-29 16:00:14 +0900

    Update script02.js
    パイソン

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,16 @@
 $(function(){
 
+  let hako = 3;
+
+  function checkBox(){
+    if(hako > 5){
+      $("#content").text(hako + "個ははいってます。箱はいっぱいです。空にします")
+      hako = 0;
+    }else if(hako <=0){
+        hako
+
+    }
+  }
 
 
 })
